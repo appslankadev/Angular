f2685b19cf0361caacf2f49c78f08e511e32301b
Selva Joy <pcfjoy@hotmail.com>
2020-06-04 01:53:21 +0200

    interpolation intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite heros are: {{myHero}} </h2>
+    <h3> my courses are {{ courses}} </h3>
+    `
 })
 export class AppComponent {
   title = 'Appslanka Dev Portal';
+  myHero = "Vijay and Ajith";
+  courses = "php, laravel, ionic, angular, android,swift"
+
+  endaHerovaKonduvaa(){
+    this.title = "vijay";
+  }
 }
