bfece0e57436141fae05ec9918019c0095049ebf
Selva Joy <pcfjoy@hotmail.com>
2020-06-04 02:31:05 +0200

    ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,24 @@ import { Component } from '@angular/core';
     <h1>{{title}}</h1>
     <h2>My favorite heros are: {{myHero}} </h2>
     <h3> my courses are {{ courses}} </h3>
+    <ul>
+    <li style="color:red;padding:20" *ngFor="let course of courses; let i = index">
+     {{ i + 1 }} {{ course }}
+    </li>
+    </ul>
+    <hr/>
+    <ol>
+    <li style="color:blue;" *ngFor="let heroName of heros">
+      {{ heroName }}
+   </li>
+   </ol>
     `
 })
 export class AppComponent {
   title = 'Appslanka Dev Portal';
   myHero = "Vijay and Ajith";
-  courses = "php, laravel, ionic, angular, android,swift"
+  heros = [ "Vijay", "Ajith", "Vikarm", "Arya", "Nayanthara"];
+  courses = ["php", "laravel", "ionic", "angular", "android","swift"]; // array
+  lists = [1, "2", 3]; // number, string, bool
 
-  endaHerovaKonduvaa(){
-    this.title = "vijay";
-  }
 }
