0a46ab9400123d677ef33c109ef00ca7d3b51ff5
Selva Joy <pcfjoy@hotmail.com>
2020-06-07 03:44:58 +0200

    added if contion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   title = 'Appslanka Dev Portal';
   user = "staff";
   heroes : string[] = ['Vijay', 'Surya', 'Vikaram', 'Rajani', 'kamal', 'karthick', 'simpu', 'sethupathi'];
+  time: number= 23;
 }
