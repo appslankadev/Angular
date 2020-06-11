fed381f3a3045c34aa597133a536006a38de279d
Selva Joy <pcfjoy@hotmail.com>
2020-06-11 02:05:29 +0200

    event in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,14 @@ export class AppComponent {
   user = "staff";
   heroes : string[] = ['Vijay', 'Surya', 'Vikaram', 'Rajani', 'kamal', 'karthick', 'simpu', 'sethupathi'];
   time: number= 23;
+  values : number = 0;
 
   clickMe(x){
     this.time = x;
+  }
+
+  calculate(event){
+    console.log(event);
+    this.values = event.target.value * 180;
   }
 }
